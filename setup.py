e9993c08161250aa9ee72bbf108f326ddcce4766
kisaragychihaya <39751846+kisaragychihaya@users.noreply.github.com>
2019-11-13 10:13:02 +0800

    Pump Version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='dobot_serial',
-    version='0.0.1',
+    version='0.0.2',
     keywords='use py3 to control dobot',
     description='Present by Virobotics',
     author='hetong',
